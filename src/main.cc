47950a1bee7546f94d46d8bea969a9eb5dd0e8b7
Pyrerune <pyrerune@gmail.com>
2019-07-25 00:01:13 -0400

    Almost Rewrote the Entire Project

--- src/main.cc
+++ src/main.cc
@@ -4,9 +4,17 @@
 #include "Interpreter.h"
 int main(int argc, char *argv[]) {
   Interpreter interpreter;
+  double version = 1.1;
   if(argv[1] == NULL) {
     std::cout << "NullFileException: No Filename Given" << std::endl << "Required At line 9" << std::endl;
     return 1;
+  }
+  if(strcmp(argv[1], "-v") == false || strcmp(argv[1], "--version") == false) {
+    std::cout << "====================" << std::endl;
+    std::cout << "========JEWL========" << std::endl;
+    std::cout << "====================" << std::endl;
+    std::cout << "Version: " << version << std::endl;
+    return 0;
   }
   std::string file = argv[1];
   std::string line;
@@ -15,8 +23,8 @@ int main(int argc, char *argv[]) {
   contents.append("<EOF>");
   char arr[contents.length() + 1];
   strcpy(arr, contents.c_str());
-  
+
   int end = interpreter.run(arr, contents.length());
-  
+
   return end;
 }
